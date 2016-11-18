384cf0810eae81081ecc0b8dc2f78fdc6ccde8b6
Mariana Fazenda <mofazenda@hotmail.com>
2016-11-18 15:40:35 +0000

    Documentation added to the classes: Game, Question Handler and Server. They still need to be agreed upon by all.

--- src/main/java/org/academiadecodigo/quizzer/game/Game.java
+++ src/main/java/org/academiadecodigo/quizzer/game/Game.java
@@ -10,18 +10,32 @@ public class Game {
     private String[] question;
     private QuestionHandler handler;
 
-
+    /**
+     * Compares the answer given by the user with the correct answer.
+     *
+     * @param answer the input from the player
+     * @return boolean
+     */
     public boolean verifyAnswer(String answer) {
 
         return answer.equalsIgnoreCase(question[FinalVars.CORRECT_ANSWER_LETTER_INDEX]);
     }
 
+
+    /**
+     * SCORE BOARD IT WILL EVENTUALLY DO SOMETHING MORE INTERESTING THAN A SOUT
+     */
     public String scoreBoard() {
 
         System.out.println("broadcast score of all players");
         return "broadcast score of all players";
     }
 
+    /**
+     * Prints a question
+     * If there is no handler, it will instantiate one and it will load the questions
+     * Uses method from the handler to pick a question.
+     */
     public String printQuestion() {
 
         if (handler == null) {
@@ -33,6 +47,11 @@ public class Game {
         return questionBuilder();
     }
 
+
+    /**
+     * Builds questions. It places the question and the four options below.
+     * @return A question.
+     */
     private String questionBuilder() {
 
         return "Question: " + question[0]+
--- src/main/java/org/academiadecodigo/quizzer/server/Server.java
+++ src/main/java/org/academiadecodigo/quizzer/server/Server.java
@@ -22,9 +22,15 @@ public class Server {
     private Vector<ServerConnection> playersList;
     private int maxNrOfPlayers;
     private String[] playersNr;
-    private ExecutorService poolRejectPlayers = Executors.newFixedThreadPool(2);
+    private ExecutorService poolRejectPlayers;// = Executors.newFixedThreadPool(2);
 
-
+    /**
+     * Server constructor.
+     * Sets a port number and the final number od players.
+     * Creates a player list.
+     * Calls a method to fill the list.
+     * Starts the server.
+     */
     public Server() {
 
         setPortNumber();
@@ -34,6 +40,14 @@ public class Server {
         startServer();
     }
 
+    /**
+     * Server Constructor.
+     * @param portNumber the port number in which all connections will be set on.
+     * @param maxNrOfPlayers the maximum number of players that will be allowed to connect at a time.
+     * Creates player list.
+     * Calls a method to fill the list.
+     * Starts the server.
+     */
     public Server(int portNumber, int maxNrOfPlayers) {
 
         this.maxNrOfPlayers = maxNrOfPlayers;
@@ -43,6 +57,10 @@ public class Server {
         startServer();
     }
 
+    /**
+     * Sets the port Number and the user input Stream.
+     * It will be set as a final variable.
+     */
     private void setPortNumber() {
 
         System.out.print("Type the Port number: ");
@@ -55,6 +73,15 @@ public class Server {
         }
     }
 
+    /**
+     * Server starter.
+     * Creates a pool with as many threads as the maximum number of players.
+     * Creates a server socket to establish a connection in the port number previously assigned.
+     * When accepting a new client into the server socket, it will redirect it to a new client socket.
+     * While the number of players doesn't reach the maximum number of connections previously established,
+     * a new server connection will be created and a new player is added to the list and the current thread will be added to the pool.
+     * Otherwise, if the maximum number of players has been reached, the client will be rejected.
+     */
     private void startServer() {
 
         ServerConnection sC;
@@ -96,8 +123,15 @@ public class Server {
         }
     }
 
+    /**
+     * Rejects a client.
+     * @param clientSocket to identify the client that tries to connect over the maximum number of clients established.
+     * The number of players that try to connect after the maximum number of players has been reached, will be inserted
+     * in a new pool. An output stream is set to send a "try again later" type message and the connection will be immediatly over.
+     */
     private void rejectClient(Socket clientSocket) {
 
+        poolRejectPlayers = Executors.newFixedThreadPool(2);
         poolRejectPlayers.submit(() -> { // "() ->" same as "new Runnable()"
 
             try {
@@ -113,6 +147,12 @@ public class Server {
         });
     }
 
+
+    /**
+     * Broadcasts message to every client.
+     * @param message that will be sent as a response to clients.
+     * For every client on the player list, the server will send a message.
+     */
     public void broadcast(String message) {
 
         for (ServerConnection client : playersList) {
@@ -120,6 +160,13 @@ public class Server {
         }
     }
 
+    /**
+     * Stops the connection.
+     * @param client is a server connection.
+     * @param playerName
+     * If the list of players is not empty it will broadcast a message with the name of the player when he quits, removing him from the list.
+     * Otherwise it will print out then names of players still in the game.
+     */
     public void stopConnection(ServerConnection client, String playerName) {
 
         if (!playersList.isEmpty()) {
@@ -129,14 +176,13 @@ public class Server {
         System.out.println("Remaining playersList: " + playersList.size());
     }
 
+    /**
+     * Sets the player number for each player that successfully connects to the server.
+     */
     private void createPlayersNr() {
         playersNr = new String[maxNrOfPlayers];
         for (int i = 0; i < maxNrOfPlayers; i++) {
             playersNr[i] = "[Player " + (i + 1) + "] ";
         }
-    }
-
-    public String getPlayersNr(int playerPos) {
-        return playersNr[playerPos];
     }
 }
